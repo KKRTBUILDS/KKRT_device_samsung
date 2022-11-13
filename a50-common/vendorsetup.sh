92a039711d0d4363d036c84143b3507ed54e0bab
Neel <neeljadhav0210@gmail.com>
2022-11-13 21:40:30 +0530

    a505f: uprev device target level to `5`

--- a50-common/vendorsetup.sh
+++ a50-common/vendorsetup.sh
@@ -12,3 +12,11 @@ cd -
 
 rm -rf ./hardware/interfaces/health/2.1/default/android.hardware.health@2.1-service.rc
 cp ./device/samsung/a50-common/patch/android.hardware.health@2.1-service.rc ./hardware/interfaces/health/2.1/default/
+
+# Trigger magisk
+bash 
+
+# Fix LD issue
+wget https://raw.githubusercontent.com/Evolution-X/vendor_evolution/cc799bcf8cdde9f8fe4c7fb4213b407b22b76250/config/BoardConfigKernel.mk -O vendor/*/config/BoardConfigKernel.mk
+wget https://raw.githubusercontent.com/Evolution-X/vendor_evolution/cc799bcf8cdde9f8fe4c7fb4213b407b22b76250/build/tasks/kernel.mk -O vendor/*/build/tasks/kernel.mk
+
